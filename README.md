d8d245e622a1b413a578fbedca2e41e31cf2954c
Dorota Kmiecik <dorota.kmiecik87@gmail.com>
2021-06-01 20:59:56 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,17 @@
+# Task-List
+## Demo: [Tasks](https://dorota87kmiecik.github.io/TasksList/)
+
+## On this page are used the basics 
+- JS
+- CSS
+- HTML 
+## This page is for providing your tasks list
+### Here is the user manual:
+
+1. First write the task you need to complete
+2. Then click on "Dodaj zadanie" button
+3. Click on the green button if your task is completed
+4. Click on the red button if you want to delete your task. 
+
+### See the presentation:
+![Lista zadań](img/Animation.gif)
---
+++ js/script.js
@@ -0,0 +1,84 @@
+{
+    const tasks = [
+
+    ];
+
+    const addNewTask = (newTaskContent) => {
+        tasks.push({
+            content: newTaskContent,
+        });
+        render();
+    };
+
+    const clearNewTaskInput = (newTaskInput) => {
+        newTaskInput.focus();
+        newTaskInput.value = "";
+    };
+
+    const removeTask = (index) => {
+        tasks.splice(index, 1);
+        render();
+    };
+
+    const toggleTaskDone = (taskIndex) => {
+        tasks[taskIndex].done = !tasks[taskIndex].done;
+        render();
+    };
+
+    const bindEvent = () => {
+
+        const removeButtons = document.querySelectorAll(".js-remove");
+        removeButtons.forEach((removeButtons, index) => {
+            removeButtons.addEventListener("click", () => {
+                removeTask(index);
+            })
+        });
+
+        const toggleDoneButtons = document.querySelectorAll(".js-done");
+        toggleDoneButtons.forEach((toggleDoneButtons, taskIndex) => {
+            toggleDoneButtons.addEventListener("click", () => {
+                toggleTaskDone(taskIndex);
+            })
+        });
+    };
+
+    const render = () => {
+        let htmlString = "";
+
+        for (const work of tasks) {
+            htmlString += `
+                <li class="task__element">
+                    <button class="task__input js-done">${work.done ? "✔" : ""}</button>
+                    <p class="task__label ${work.done ? "task__label--done" : ""}"> ${work.content} </p>
+                    <button class="task__button js-remove">🗑</button>
+                </li>        
+          `;
+        }
+
+        document.querySelector(".js-tasks").innerHTML = htmlString;
+        bindEvent();
+    };
+
+    const onFormSubmit = (event) => {
+        event.preventDefault();
+
+        const newTaskInput = document.querySelector(".js-newTask");
+        const newTaskContent = newTaskInput.value.trim();
+
+        if (newTaskContent === "") {
+            newTaskInput.focus();
+        } else {
+            addNewTask(newTaskContent);
+            clearNewTaskInput(newTaskInput);
+        }
+    };
+
+    const init = () => {
+        render();
+
+        const form = document.querySelector(".js-form");
+        form.addEventListener("submit", onFormSubmit)
+
+    };
+    init();
+}
